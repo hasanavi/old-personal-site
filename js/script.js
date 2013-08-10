dab39857fd27cc123f06b3192fb6cc1320b9a799
Hasan Haque <hasan@movementlondon.com>
2013-08-10 23:13:58 +0100

    update loader

--- js/script.js
+++ js/script.js
@@ -3,20 +3,26 @@ $(function(){
 	$('#menu li').click(function(){
 		var page = $(this).attr('id');
 		if((typeof page != 'undefined') && (page.length > 0)){
-			$('#loader-anim').show();
-			$('#loader-anim').css('width','100%');
+			showLoader();
 			$.get('ajax-pages/'+page+'.html',function(res){
 				$('#col-right').html(res);
-				$('#loader-anim').hide();
-				$('#loader-anim').css('width','0');
+				hideLoader();
 			});
 		}else{
-			$('#loader-anim').show();
-			$('#loader-anim').css('width','100%');
-			setTimeout(function(){
-				$('#loader-anim').hide();
-				$('#loader-anim').css('width','0');
-			},500);
+			showLoader();
+			hideLoader();			
 		}
 	});
+
+	function showLoader () {
+		$('#loader-anim').show();
+		$('#loader-anim').css('width','100%');
+	}
+
+	function hideLoader () {
+		setTimeout(function(){
+				$('#loader-anim').hide();
+				$('#loader-anim').css('width','0');
+		},500);
+	}
 });

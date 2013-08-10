d296669c26f0f6d875addf2eb90d66f024f0a529
Hasan Haque <hasan@movementlondon.com>
2013-08-10 23:11:16 +0100

    various fixes

--- js/script.js
+++ js/script.js
@@ -3,11 +3,20 @@ $(function(){
 	$('#menu li').click(function(){
 		var page = $(this).attr('id');
 		if((typeof page != 'undefined') && (page.length > 0)){
+			$('#loader-anim').show();
 			$('#loader-anim').css('width','100%');
 			$.get('ajax-pages/'+page+'.html',function(res){
 				$('#col-right').html(res);
+				$('#loader-anim').hide();
 				$('#loader-anim').css('width','0');
 			});
+		}else{
+			$('#loader-anim').show();
+			$('#loader-anim').css('width','100%');
+			setTimeout(function(){
+				$('#loader-anim').hide();
+				$('#loader-anim').css('width','0');
+			},500);
 		}
 	});
 });

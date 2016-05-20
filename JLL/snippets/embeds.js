10bb2bd0a5d95d216f95f6193ccead6892c9f67b
Hasan Azizul Haque <hasanavi@gmail.com>
2016-05-20 16:02:09 +0100

    Update embeds.js

--- JLL/snippets/embeds.js
+++ JLL/snippets/embeds.js
@@ -2,11 +2,11 @@
 
 	//$(function() {
 		var $surveyContainer = $('.survey-container'),
-			cssFile = 'embed-style.css',
+			cssFile = 'https://rawgit.com/hasanavi/hasanavi.github.io/master/JLL/snippets/embed-style.css',
 			img = new Image(),
 			position = $surveyContainer.data('pos');
 
-		img.src = 'survey-img.png';
+		img.src = 'https://raw.githubusercontent.com/hasanavi/hasanavi.github.io/master/JLL/snippets/survey-img.png';
 		img.alt = "click here for survey";
 			
 		$surveyContainer.addClass(position);
